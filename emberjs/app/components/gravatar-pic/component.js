2431ffe87add9195994ad10ea01eebb8db7562e8
jangkoo <le.khanh@goldenequator.com>
2017-01-03 19:15:40 +0800

    latest

--- emberjs/app/components/gravatar-pic/component.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  // didInitAttrs() {
-  //   console.log('>>>>> foo');
-  //   this.attrs.sha = myShaFunc(this.attrs.email);
-  //   this.attrs.sha = 'testies';
-  // }
-});
--- emberjs/app/components/service-item/component.js
+++ emberjs/app/components/service-item/component.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['col-sm-6', 'col-md-4']
 });
