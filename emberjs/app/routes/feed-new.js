0708158febfd9e434b1e760813608e3a6a3fd8af
jangkoo <le.khanh@goldenequator.com>
2017-01-04 19:05:24 +0800

    feed

--- emberjs/app/routes/feed-new.js
+++ emberjs/app/routes/feed-new.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+model() {
+    return Ember.RSVP.hash({
+      headerItem: {title: 'Complete trigger fields', step:3}
+    });
+  }
 });
